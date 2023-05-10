bb5bc1158a77b87b33e0b87898047ab06433109d
eneskaracaa0 <eneskaraca18.ek@gmail.com>
2023-05-11 02:41:53 +0300

    API ANLATIM

--- app.js
+++ app.js
@@ -10,7 +10,18 @@ let getJoke = () => {
         .then((item) => {
             jokeContainer.textContent = `${item.joke}`;
             jokeContainer.classList.add("fade");
+
         });
+
 };
 btn.addEventListener("click", getJoke);
 getJoke();
+
+
+//API KULLANIMI:
+/*
+1-İlk olarak istekte bulunmak istediğimiz adresi değişkene atadık.
+2-fetch(url) ile isteğimizi yaptık.
+3-Sonrasında sonuç Promise olarak döndüğü için then ile sonucu yakalıyoruz.Biz burada data diye değişken adıyla sonucumuzu yakaladık ve json bilgisini almak için data.json dedik.
+4-Yeni bir then ifadesi ile json olarak gelen bilgiyi yakalıyoruz ve bu bilgiyi item değişkenine atıyoruz. item değişkeninde bize gelen json bilgisi yer alıyor.joke içeriğini text.content metoduyla değiştirdik.
+*/
